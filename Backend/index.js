83c082da76cf58cd3d40749ff584c44db938b0ed
Lautaro Urso <lautarourso@gmail.com>
2025-07-06 17:30:30 -0300

    pie

--- Backend/index.js
+++ Backend/index.js
@@ -17,3 +17,16 @@ app.use("/vids", VideosRouter);
 app.listen(process.env.PORT || 9000, () =>
     console.log(`Server is running on port ${process.env.PORT || 9000}` + ", Ya puedes empezar")
 );
+
+/*
+"nombre": "a",
+"apellido": "a",
+"email": "metricafutbolera@gmail.com",
+"dni": 23877647,
+"password": "piepie"
+
+
+"email": "lautarourso@gmail.com",
+"password": "Lautaro"
+
+*/
