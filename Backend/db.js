766c750f22d5b52a425604c9e46f084510a213a1
Lautaro Urso <73238286+Lautarourso@users.noreply.github.com>
2025-08-05 11:29:14 -0300

    ponele

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const config = {
-    url: "psql 'postgresql://neondb_owner:npg_8AKpoRnSYN0x@ep-green-dawn-acc9cczp-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'",
-    user: "LAUTARO", // Tu usuario de base de datos
-    host: "ep-green-dawn-acc9cczp-pooler.sa-east-1.aws.neon.tech", // El host de la base de datos
-    database: "neondb", // El nombre de tu base de datos
-    password: "npg_8AKpoRnSYN0x", // La contraseña de tu base de datos
-    port: 3000, // El puerto de PostgreSQL (por defecto es 5432)
-    ssl: true, // SSL habilitado para conexiones seguras
-};
-
-
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(process.env.DB_URL, {
@@ -22,12 +11,13 @@ export const sequelize = new Sequelize(process.env.DB_URL, {
       rejectUnauthorized: false,
     },
   },
-  logging: false,
+  logging: false, // Opcional: desactiva logs de SQL en consola
 });
 
+// Probar conexión
 try {
   await sequelize.authenticate();
-  console.log("Connection has been established successfully.");
+  console.log("Conexión con la base de datos establecida correctamente.");
 } catch (error) {
-  console.error("Unable to connect to the database:", error);
+  console.error("No se pudo conectar con la base de datos:", error);
 }
