e6a753b613c301dc84633fc8e81649e62f86a5d7
ValenKono <147621279+ValenKono@users.noreply.github.com>
2025-08-18 00:13:12 -0300

    primpt

--- IA/Analisis.py
+++ IA/Analisis.py
@@ -1,15 +1,16 @@
-def construir_prompt_completo(tipo_falla, ubicacion, longitud, profundidad_min, profundidad_max, angulo_max, delga, tiempo_impacto):
+def construir_prompt_completo(tipo_falla, ubicacion, longitud,profundidad_min, profundidad_max,delga, tiempo_impacto, espesor_gasoducto, presion_gas, zona, material_gasoducto):
     ejemplo = """
 Ejemplo de informe de ingeniería:
 
 1. Tipo de falla: Abolladura
 2. Ubicación: 123.4 m
-3. Longitud: 0.5 m
-4. Profundidad: 6.2 mm
-5. Gravedad: Alta (por profundidad > 20% del espesor)
-6. Recomendación: Reemplazo o encamisado urgente.
+3. Longitud: 500 mm
+4. Profundidad: 1.3 mm
+5. Espesor gasoducto: 12.7
+5. Gravedad: Moderada
+6. Recomendación: Monitoreo de la situación en 3 meses. Si se agrava, reemplazar tramo.
 
-Reglas técnicas:
+
 
 
 Fin del ejemplo.
@@ -19,16 +20,20 @@ Fin del ejemplo.
 Actuá como un ingeniero especialista en inspección de gasoductos.
 Generá un informe técnico respetando el formato y reglas del ejemplo siguiente:
 
+
 {ejemplo}
 
 Datos del caso actual:
 - Tipo de falla: {tipo_falla}
 - Ubicación: {ubicacion} metros
-- Longitud estimada: {longitud} metros
+- Longitud estimada: {longitud} mm
 - Profundidad: entre {profundidad_min} mm y {profundidad_max} mm
-- Ángulo máximo de impacto: {angulo_max} grados
 - Delga afectada: {delga}
-- Duración del impacto: {tiempo_impacto} segundos
+- Duración del impacto: {tiempo_impacto} ms
+- Espesor del gasoducto: {espesor_gasoducto} mm
+- Presion habitual del gas: {presion_gas} bares
+- Zona: {zona}
+- Material: {material_gasoducto}
 
 Redactá el informe final, claro y profesional.
 """
--- IA/prompt.py
+++ IA/prompt.py
@@ -4,19 +4,23 @@ from Analisis import construir_prompt_completo
 client = genai.Client(api_key="AIzaSyBWJ0hPb9E5G54J74kpQjWeDm-P2SpicJQ")
 
 tipo_falla = "Abolladura"
-ubicacion = 123.4
-longitud = 0.5
-profundidad_min = 6.2
-profundidad_max = 7.8
-angulo_max = 45
-delga = "D12"
-tiempo_impacto = 0.8
+ubicacion = 123.4 
+longitud = 500 
+profundidad_min = 2.3
+profundidad_max = 3.8 
+delga = "D4"
+tiempo_impacto = 800 
+espesor_gasoducto = 12.7 
+presion_gas = 16 
+zona = "despoblada"
+material_gasoducto = "Acero al carbono de alta resistencia"
 
 # Construir el prompt
 prompt = construir_prompt_completo(
     tipo_falla, ubicacion, longitud,
     profundidad_min, profundidad_max,
-    angulo_max, delga, tiempo_impacto
+    delga, tiempo_impacto, espesor_gasoducto, 
+    presion_gas, zona, material_gasoducto   
 )
 
 # Enviar a Gemini
