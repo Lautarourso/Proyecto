10e7db7a43b806f4a952d7a011589b252f4e7996
ValenKono <147621279+ValenKono@users.noreply.github.com>
2025-08-19 12:01:06 -0300

    DatosJoanGarcia

--- IA/Datos.py
+++ IA/Datos.py
@@ -1,6 +1,13 @@
-def longitud_falla(tiempo_impacto_segundos, velocidad_m_s):
-    return tiempo_impacto_segundos * velocidad_m_s  
+# distancia_min_mm = 4
+# distancia_max_mm = 5
+# distancia_imp_mm = distancia_max_mm - distancia_min_mm
+tiempo_imp_ms = 300
+tiempo_imp_s = tiempo_imp_ms * 100
+# velocidad_imp = distancia_imp_mm/tiempo_imp_ms
+velocidad_imp_MS = 4
 
-def profundidad_falla(angulo_grados):
-    profundidad_mm = (angulo_grados / 30.0) * 10.0
-    return profundidad_mm
+def longitud_falla(tiempo_imp_s, velocidad_imp_MS):
+    return tiempo_imp_s * velocidad_imp_MS  
+
+
+
