f810eb0c1549a52d54e8ec3291e67410e8944f70
Lautaro Urso <73238286+Lautarourso@users.noreply.github.com>
2025-09-19 15:35:45 -0300

    sds

--- Backend/controllers/proyectos.controller.js
+++ Backend/controllers/proyectos.controller.js
@@ -23,15 +23,13 @@ export const UploadP = async (req, res) => {
           return res.status(500).json({ message: "Error subiendo a Cloudinary" });
         }
     });
-    const tipo_mime= result.resource_type
-
-    const url= result.secure_url
+    
     const proyecto = await proyectosService.createProyectos(
       req.idUsuario,
       analisisData,
       videoData,
-      url,
-      tipo_mime,
+      url= result.secure_url,
+      tipo_mime= result.resource_type,
       req.file
     );
 
