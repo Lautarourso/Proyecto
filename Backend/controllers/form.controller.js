072c09dd2b51fec2a44e1b9ffb6f3ae8089dd91f
Lautaro Urso <73238286+Lautarourso@users.noreply.github.com>
2025-11-03 17:02:33 -0300

    ahora si

--- Backend/controllers/form.controller.js
+++ Backend/controllers/form.controller.js
@@ -3,12 +3,15 @@ import formservice from "../services/form.service.js";
 const IA = async (req, res) => {
     const { proyectoId, datosMaterial } = req.body;
 
+    
+
+  try {
+
     const usuario2 = await formservice.getFormbyIDP(proyectoId);
 
     if (usuario2)
         return res.status(400).json({ message: "Ya hay una cuenta registrada con ese correo" });
-
-  try {
+        
     await formservice.Cañerias({
         proyecto_id: proyectoId,
         ...datosMaterial,
