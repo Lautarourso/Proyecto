37921227efecfab2d9e6bf630b6c7de4e660ac7c
Lautaro Urso <73238286+Lautarourso@users.noreply.github.com>
2025-11-04 12:01:21 -0300

    Esto lo hizo Konoba

--- IA/Datos.py
+++ IA/Datos.py
@@ -1,9 +1,8 @@
 import json
 from Numericos import ruta_salida
+from DatosForm import ruta_salida
 
 
-
-id_falla = 49  
 
 # velocidad_imp = distancia_imp_mm/tiempo_imp_ms
 velocidad_imp_MS = 4
@@ -19,41 +18,33 @@ def longitud_falla(tiempo_imp_ms_param, velocidad_imp_MS):
     return longitud_m * 1000
 
 
-with open(ruta_salida, "r", encoding="utf-8") as f:
-    casos = json.load(f)
 
-for caso in casos:
-      if caso.get("id") == id_falla:
-        caso_a_analizar = caso
-        break
+tiempo_imp_ms = ["tiempo"]
+duracion_ms = ["distancia"]
 
 
-if caso_a_analizar:     
-    tiempo_imp_ms = caso_a_analizar["tiempo"]
-    duracion_ms = caso_a_analizar["distancia"]
 
-
-    longitud_calculada = longitud_falla(tiempo_imp_ms, velocidad_imp_MS)
-
-    print(f"ID Encontrado: {id_falla}")
+    
 
 
 
 
-tipo_falla= "Abolladura"
+
+
+tipo_falla= ["falla"]
 ubicacion= 20.3
 longitud = longitud_falla(tiempo_imp_ms, velocidad_imp_MS)
 profundidad_max=1.5      
-espesor_gasoducto=12.7 
-diametro_gasoducto=100
-tmfe = 241 
-presion_gas=16
-latitud_in = -37.334044 
-longitud_in = -59.145896
-latitud_fin = -37.332270  
-longitud_fin = -59.136610 
-material_gasoducto="Acero API 5L Grado B"
-antiguedad_gasoducto=10
-presion_max_op = 32
+espesor_gasoducto=["espesor"] 
+diametro_gasoducto=["diametro"]
+tmfe = ["tfme"] 
+presion_gas=["presionHabitual"]
+latitud_in = ["latitudInicial"] 
+longitud_in = ["longitudInicial"] 
+latitud_fin = ["latitudFinal"] 
+longitud_fin = ["longitudFinal"]  
+material_gasoducto=antiguedad_gasoducto=["material"]
+antiguedad_gasoducto=["antiguedad"]
+presion_max_op = ["presionMaxima"]
 
 
--- IA/DatosForm.py
+++ IA/DatosForm.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 # Cargar credenciales del archivo .env
 load_dotenv()
-id = 1
+id = 11
 # URLs de la API
 login_url = "https://proyecto-zvzl.onrender.com/auth/login"
 form_url = f"https://proyecto-zvzl.onrender.com/form/{id}"
