c1c842afaf3392083fb8137ecd56b20049532743
Lautaro Urso <73238286+Lautarourso@users.noreply.github.com>
2025-11-11 10:00:26 -0300

    ranzo"

--- Backend/controllers/form.controller.js
+++ Backend/controllers/form.controller.js
@@ -42,26 +42,27 @@ export const getF = async (req, res) => {
   };
 
 
-export const python = async (req, res) => {
-  const { id, token } = req.body;
-  console.log("ID recibido desde el front:", id);
-
-  const scriptPath = path.join(process.cwd(), "IA", "modulo.py");
-  console.log("Ruta del script Python:", scriptPath);
-  try {
-  PythonShell.run(scriptPath, { args: [id, token] })
-  const informeFinal = results[results.length - 1]; 
-        
-        // 4. Devolver la respuesta exitosa con el informe
-        return res.status(200).json({ 
-            status: "success", 
-            message: "Datos guardados e informe de IA generado.",
-            informe: informeFinal, // Enviamos el resultado de la IA al Front-End
-        });
-
-    } catch (err) {
-        // Error durante la ejecución del script Python (falla en el script, en la IA, etc.)
-        return res.status(500).json({ error_generacion: `Fallo al ejecutar el script Python: ${err.message}` });
-    }
-};
+  export const python = async (req, res) => {
+    const { id, token } = req.body;
+    console.log("ID recibido desde el front:", id);
+  
+    const scriptPath = path.join(process.cwd(), "IA", "modulo.py");
+    console.log("Ruta del script Python:", scriptPath);
+  
+    PythonShell.run(scriptPath, { args: [id, token] }, (err, results) => {
+      if (err) {
+        console.error("Error ejecutando Python:", err);
+        return res.status(500).json({ error_generacion: err.message });
+      }
+  
+      const informeFinal = results[results.length - 1];
+  
+      return res.status(200).json({
+        status: "success",
+        message: "Datos guardados e informe de IA generado.",
+        informe: informeFinal,
+      });
+    });
+  };
+  
 export default { IA, getF, python};
