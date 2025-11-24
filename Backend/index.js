b366e1298d07f3b83942aa9a730fc78bc6f726f5
ValenKono <147621279+ValenKono@users.noreply.github.com>
2025-11-24 17:39:21 -0300

    Davanbte

--- Backend/index.js
+++ Backend/index.js
@@ -38,9 +38,16 @@ async function startServer() {
 
 
   const PORT = process.env.PORT || 9000;
-  app.listen(PORT, "0.0.0.0", () =>
+  const server = app.listen(PORT, "0.0.0.0", () =>
     console.log(`Server is running on port ${PORT}, Ya puedes empezar`)
   );
+
+  server.timeout = 60000; // 60 segundos (60,000 milisegundos)
+  
+  // Opcional: Aumentar el keepAliveTimeout
+  server.keepAliveTimeout = 65000; // 65 segundos
+
+
 }
 
 startServer().catch((err) => {
